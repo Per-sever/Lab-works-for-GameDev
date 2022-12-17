1e3ecc5f74fa06c0424cf0bf55174b63874bb033
per-sever <a.petrin107@gmail.com>
2022-12-17 20:21:41 +0300

    adding 4.4 lab

--- My project/Assets/PlayerMovment.cs
+++ My project/Assets/PlayerMovment.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
-    public float moveSpeed = 4f;
+    public float moveSpeed = 3f;
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -16,5 +17,19 @@ public class PlayerMovment : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
+
+        if (Input.GetAxis("Horizontal") < 0) {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (Input.GetAxis("Horizontal") > 0) {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+          if (Input.GetAxis("Horizontal") != 0) {
+            anim.SetBool("isRun", true);
+        }
+        if (Input.GetAxis("Horizontal") == 0) {
+            anim.SetBool("isRun", false);
+        }
     }
 }
