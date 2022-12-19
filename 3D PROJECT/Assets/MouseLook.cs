ade1f6ab50146320d4f8d6ebb4835ffbb5dcfe43
per-sever <a.petrin107@gmail.com>
2022-12-20 00:39:04 +0300

    adding few menus to 6.4 lab

--- 3D PROJECT/Assets/MouseLook.cs
+++ 3D PROJECT/Assets/MouseLook.cs
@@ -8,16 +8,24 @@ public class MouseLook : MonoBehaviour
     public float mouseSensivity = 100f;
     public Transform playerBody;
 
+    public GameObject canvas;
+
     float xRotation = 0f;
     // Start is called before the first frame update
     void Start()
     {
+        canvas = GameObject.FindGameObjectWithTag("Canvas");
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            canvas.gameObject.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+        }
         float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
 
@@ -27,4 +35,10 @@ public class MouseLook : MonoBehaviour
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
+
+    public void ReturnToGame() {
+        Cursor.lockState = CursorLockMode.Locked;
+        canvas.gameObject.SetActive(false);
+    }
+
 }
